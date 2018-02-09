8c7d310d6353becc97ea9e8ca962ac9621686245
Sergei St <stro1996@gmail.com>
2018-02-09 22:01:22 +0300

    add edit item

--- app/controller/itemController.js
+++ app/controller/itemController.js
@@ -3,7 +3,7 @@ const Item    = require('../model/itemCardModel');
 module.exports = {
   add: (req, res) => {
     let newItem = new Item(req.body);
-    newItem.createdBy = req.user.fullName;
+    newItem.createdBy = req.user.email;
     newItem.save((err, item) => {
       if (err) {
         return res.sendStatus(400)
@@ -24,5 +24,39 @@ module.exports = {
   },
 
   getMy: (req, res) => {
+    Item.find({ createdBy: req.user.email}, (err, result) => {
+      if (err) {
+        return res.sendStatus(400)
+      }
+      return res.status(200).json({
+        item: result
+      })
+    })
+  },
+
+  edditItem: (req, res) => {
+    if (!req.body.id) {
+      return res.sendStatus(400);
+    }
+    Item.findOne({ _id: req.body.id }, (err, result) => {
+      if (err) {
+        return res.sendStatus(500);
+      }
+      if (result.createdBy === req.user.email) {
+        result.name = req.body.name || result.name;
+        result.discription = req.body.discription || result.discription;
+
+        result.save((err, result) => {
+          if (err) {
+            return res.sendStatus(500);
+          }
+          return res.status(200).json({
+            newElement: result
+          })
+        })
+      }else {
+        return res.sendStatus(500);
+      }
+    })
   }
 }

4674395e24f44939ce4c7321578903185a764aa5
Sergei Tarasevich <sergeitarasevich@MacBook-Pro-Sergei.local>
2018-09-02 19:02:31 +0300

    add delte and refator code

--- app/routes/itemCardRouts.js
+++ app/routes/itemCardRouts.js
@@ -1,15 +1,13 @@
 const itemCardController = require('../controller/itemController');
 
 module.exports = app => {
-  app.route('/item_card/created')
-    .post(itemCardController.add);
+  app.route('/item_card')
+    .post(itemCardController.add)
+    .put(itemCardController.edditItem)
+    .delete(itemCardController.deleteItem)
+    .get(itemCardController.getAll);
 
   app.route('/item_card/get_my')
     .get(itemCardController.getMy);
 
-  app.route('/item_card/edit')
-    .post(itemCardController.edditItem);
-
-  app.route('/item_card')
-    .get(itemCardController.getAll);
 };
